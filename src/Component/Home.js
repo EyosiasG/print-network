10678486c183aad322aec0e43a4babc5aca9bd71
elias <datwii1234@gmail.com>
2023-11-24 12:31:04 +0300

    new

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -45,28 +45,39 @@ const ItemsList = () => {
   }, [db]); // Include db as a dependency to avoid useEffect warnings
 
   return (
-    <div>
-      <h2 className="mb-12">Items List</h2>
+
+    <div className="container mx-auto">
+      <h2 className="text-2xl font-bold mb-8">Items List</h2>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <div className="grid grid-cols-3 gap-2">
-        {Object.entries(items).map(([itemName, imageLink]) => (
-          <Link key={itemName} to={`/item-details/${itemName}`} className="mb-4 p-4">
-            {/* Clickable item with details */}
-            <img
-              src={imageLink}
-              alt={`${itemName} Image`}
-              className="w-72 h-80 max-w-20 max-h-80 mb-2"
-            />
-            <strong className="block">{itemName}:</strong>
-          </Link>
-        ))}
-      </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {Object.entries(items).map(([itemName, imageLink]) => (
+            <div
+              key={itemName}
+              to={`/item-details/${itemName}`}
+              className="bg-white rounded-lg shadow-md p-6 flex flex-col items-center justify-center transition duration-300 transform hover:scale-105"
+            >
+              {/* Clickable item with details */}
+              <img
+                src={imageLink}
+                alt={`${itemName} Image`}
+                className="w-64 h-64 object-cover mb-4"
+              />
+              <strong className="text-xl text-center">{itemName}</strong>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4">
+            Click me
+          </button>
+            </div>
+          ))}
+
+        </div>
       )}
     </div>
-  );
-};
+      )}
+    
+  
+
 
 export const Home = () => {
   return (
